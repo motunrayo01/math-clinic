37393b91e01ad7305598352000520a0170aac832
olayinka02 <64751867+olayinka02@users.noreply.github.com>
2020-07-30 11:56:23 +0100

    math-clinic
    This was designed with HTML, CSS ,Bootsrap and Javascript

---
+++ math js/other.js
@@ -0,0 +1,22 @@
+function text(){
+    var comment=prompt("Please! Enter your comment");
+    if(comment!==""){
+        alert("we will get back to you base on your comment ...THANKS")
+    }else if(comment==""){
+        alert("input a simple comment before you press OK");
+        var comment1=prompt("Please! Enter your comment");
+        if(comment1!==""){
+            alert("we will get back to you base on your comment ...THANKS")
+        }else if(comment1==""){
+            alert("..EXITS..")
+        };
+    };
+};
+
+function wat(){
+    alert("Please! Login through the whatsapp App\n and search for 08101831001,07035764622");
+};
+
+function twi(){
+    alert("Please! Login through the Twitter App\n and search for olayinka01");
+}
