5a56fdd2c29e4cc1081728ce991f93a5b74c2f1d
olayinka02 <64751867+olayinka02@users.noreply.github.com>
2020-07-31 12:05:53 +0100

    math-clinic
    This was designed with HTML, CSS and Javascript

--- math js/index.js
+++ math js/index.js
@@ -1,31 +1,22 @@
-function sum(){
-  
-  var Email=document.myForm.email.value;
-    var Email1=document.myForm.email.value.indexOf("@");
-    var Email2=document.myForm.email.value.lastIndexOf(".");
-    var password1=document.myForm.password1.value;
-    var password2=document.myForm.password2.value;
-    
-
- 
-   if( password1=="" || password2=="" ){
-    alert("please enter your password\n" + "password cant be empty" );
-     document.myForm.password1.focus();
-     document.myForm.password2.focus();
-     return false;
-   }else if(Email1<1 || ((Email2-Email1) > 6) ||Email==""){
-    alert("please enter correct EmailID,\n"+ Email + " is not correct"  );
-    document.myForm.email.focus();
-    return false;
- }
-  else if(password1!==password2){
-    alert( "'" + password1+ "'" +" dosnt match " + "'" + password2 + "'" +"in your password."  );
-    document.myForm.password1.focus();
-    document.myForm.password2.focus();
-   return false;
-   }else{
-     window.location="https://olayinka02.github.io/bible-quiz/";
-   }
-   
-
+function text(){
+    var comment=prompt("Please! Enter your comment");
+    if(comment!==""){
+        alert("we will get back to you base on your comment ...THANKS")
+    }else if(comment==""){
+        alert("input a simple comment before you press OK");
+        var comment1=prompt("Please! Enter your comment");
+        if(comment1!==""){
+            alert("we will get back to you base on your comment ...THANKS")
+        }else if(comment1==""){
+            alert("..EXITS..")
+        };
+    };
 };
+
+function wat(){
+    alert("Please! Login through the whatsapp App\n and search for 08101831001,07035764622");
+};
+
+function twi(){
+    alert("Please! Login through the Twitter App\n and search for olayinka01");
+}
